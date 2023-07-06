416309fe11c3d0c51137e2e61c515373ee6fe6de
HuseynFarzali <huseynyf@code.edu.az>
2023-07-06 02:48:01 -0700

    Add project files.

---
+++ Task_DateTime/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Task_DateTime
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            Console.Write("Enter the date in the given format --> dd_MM_yyyy: ");
+            string stringDate = Console.ReadLine();
+
+            try
+            {
+                DateTime date = DateTime.ParseExact(stringDate, "dd_MM_yyyy", CultureInfo.InvariantCulture);
+                Console.WriteLine("Entered date: " + date.ToString("dd/MMMM/yyyy"));
+            }
+
+            catch(FormatException ex)
+            {
+                Console.WriteLine($"{ex.GetType()}: {ex.Message}");
+            }
+        }
+
+        static void Task2()
+        {
+            Console.Write("Enter the date in the given format --> dd/MM/yyyy: ");
+            string stringDate = Console.ReadLine();
+
+            DateTime date = DateTime.ParseExact(stringDate, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            TimeSpan timespan = new TimeSpan(days: 30, hours: 6, minutes: 17, seconds: 0);
+
+            date = date.Add(timespan);
+
+            Console.Write("Final date: " + date.ToString("dd/MM/yyyy hh:mm:ss"));
+        }
+
+        static void Main(string[] args)
+        {
+            //Task1();
+            //Task2();
+        }
+    }
+}
